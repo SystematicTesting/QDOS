1ce4358b2268359a0e229173cf3fb588bc344633
SelFIT <info@innovatetesting.com>
2017-11-19 23:41:08 +0000

    Added New Keyword Partial Link Text - Initial Draft

---
+++ st-qdos-core/src/main/java/com/systematictesting/automation/core/keywords/impl/PartialLinkText.java
@@ -0,0 +1,21 @@
+package com.systematictesting.automation.core.keywords.impl;
+
+import java.util.Map;
+
+import com.systematictesting.automation.core.keywords.Process;
+import com.systematictesting.qdos.beans.TestStepData;
+
+public class PartialLinkText implements Process {
+
+	@Override
+	public String execute(Map<String, String> elementKeyValuePairs, TestStepData testStep) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
